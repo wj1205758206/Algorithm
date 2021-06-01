631484e99aa29ff0dbd3cabdb008998b3dcb3010
weijian <1205758206@qq.com>
2021-06-01 09:53:13 +0800

    链表节点结构

---
+++ src/LinkedList/ListNode.java
@@ -0,0 +1,41 @@
+package LinkedList;
+
+/**
+ * 单链表节点的结构
+ */
+public class ListNode {
+
+    private int val;
+    private ListNode next = null;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+
+
+
+}
